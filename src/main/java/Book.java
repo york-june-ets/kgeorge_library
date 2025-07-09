9dbec5fd16c4d1c2d026d9aa040e74d029021d55
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-09 12:36:08 -0400

    book getter and setter methods

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -4,17 +4,51 @@ import java.util.List;
 public class Book {
     private String title;
     private List<Author> authors;
-    private Date datePublished;
-    private String rented;
+    private int year;
+    private boolean rented;
 
-    public Book(String title, List<Author> author, Date datePublished) {
+    public Book(String title, List<Author> author, int year) {
         this.title = title;
         this.authors = author;
-        this.datePublished = datePublished;
+        this.year= year;
+        this.rented = false;
     }
 
     // GETTERS
+    public String getTitle() {
+        return title;
+    }
+
     public List<Author> getAuthors() {
         return authors;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isRented() {
+        return rented;
+    }
+
+    // SETTERS
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setRented() {
+        this.rented = true;
+    }
+
+    public void setReturned() {
+        this.rented = false;
     }
 }
