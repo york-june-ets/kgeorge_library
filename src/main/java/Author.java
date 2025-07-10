d567f7feabe43cf2b7d39e50601431620db44f35
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-10 12:20:10 -0400

    updated author class

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -1,32 +1,53 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 
 public class Author {
-    private String name;
-    private List<Book> books;
+    private final int ID;
+    private final String name;
+    private String bio;
+    private final String gender;
+    private final LocalDate birthDate;
+    private LocalDate deathDate;
 
-    public Author(String name) {
+    public Author(int ID, String name, String bio, String gender, LocalDate birthDate, LocalDate deathDate) {
+        if (name == null || birthDate == null) {
+            throw new IllegalArgumentException("Author name and birth date are required");
+        }
+        this.ID = ID;
         this.name = name;
-        this.books = new ArrayList<Book>();
+        this.bio = bio;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
+
+    }
+    // SETTERS
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    public void setDeathDate(LocalDate deathDate) {
+        this.deathDate = deathDate;
     }
 
     // GETTERS
+    public int getID() {
+        return ID;
+    }
     public String getName() {
         return name;
     }
-
-    public List<Book> getBooks() {
-        return books;
+    public String getBio() {
+        return bio;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public LocalDate getBirthDate() {
+        return birthDate;
     }
 
-    // SETTERS
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void addBook(Book book) {
-        if (!this.books.contains(book)) {
-            this.books.add(book);
-        }
+    // PRINT
+    @Override
+    public String toString() {
+        return "Author: " + this.name;
     }
 }
